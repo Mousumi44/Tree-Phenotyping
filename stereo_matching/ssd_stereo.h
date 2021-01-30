8cbdc5c0cc5c768b94035fa2cfd4613c4843adb2
Mousumi44 <mousumiakter5570@gmail.com>
2021-01-30 09:59:08 -0600

    v3

--- stereo_matching/ssd_stereo.h
+++ stereo_matching/ssd_stereo.h
@@ -2,22 +2,17 @@
 #include <stdio.h>
 #include <iostream>
 
-
 using namespace cv;
 using namespace std;
 class Stereo {
 private:
-	int win_size_, max_disparity_, tran_win_size_;
+	int win_size_, max_disparity_;
 public:
 	
-	Stereo(int window_size, int max_dis, int tran_size) {
+	Stereo(int window_size, int max_dis) {
 		win_size_ = window_size;  // block size 
 		max_disparity_ = max_dis;
-		tran_win_size_ = tran_size; // matching cost windows size
 	}
 
-	Mat transform(Mat image, int tran_size);
-	Mat census_transform(Mat image, int tran_size);
 	Mat stereo_match(Mat left, Mat right);
-	Mat stereo_match_parallel(Mat left, Mat right);	
 };
--- stereo_matching/stereo_matching.cpp
+++ stereo_matching/stereo_matching.cpp
@@ -1,40 +1,12 @@
 #include "ssd_stereo.h"
 
-Mat Stereo::transform(Mat image, int windowsize) {
-	int h = image.rows;
-	int w = image.cols;
-	Mat imgDisparity8U = Mat(image.rows, image.cols, CV_8U);
-	int window_half = windowsize / 2;
-
-	for (int y = window_half; y < h - window_half; ++y) {
-		for (int x = window_half; x < w - window_half; ++x) {
-			int ssd = 0;
-
-			for (int v = -window_half; v < window_half + 1; ++v) {
-				for (int u = -window_half; u < window_half + 1; ++u) {
-
-					if (image.at<uchar>(y + v, x + u) > image.at<uchar>(y, x)) ++ssd;
-				}
-
-			}
-
-			imgDisparity8U.at<uchar>(y, x) = ssd;
-
-		}
-	}
-	return imgDisparity8U;
-}
-
-
+// Block Matching Algorithm
 Mat Stereo::stereo_match(Mat left, Mat right) {
 	int h = left.rows;
 	int w = left.cols;
 	Mat imgDisparity8U = Mat(left.rows, left.cols, CV_8U);
 	int window_half = win_size_ / 2;
 	int adjust = 255 / max_disparity_;
-
-	left = Stereo::transform(left, tran_win_size_);
-	right = Stereo::transform(right, tran_win_size_);
 
 	for (int y = window_half; y < h - window_half; ++y) {
 		uchar *imgDisparity_y = imgDisparity8U.ptr(y);
@@ -72,19 +44,18 @@ Mat Stereo::stereo_match(Mat left, Mat right) {
 int main(int argc, char** argv)
 {
 	cv::CommandLineParser parser(argc, argv,
-		"{left||}{right||}{max-disparity|0|}{window_size|0|}{tranwin_size|0|}{output||}");
+		"{left||}{right||}{max-disparity|0|}{window_size|0|}{output||}");
 	string leftname = parser.get<std::string>("left");
 	string rightname = parser.get<std::string>("right");
 	string outname = parser.get<std::string>("output");
 	int max_disparity = parser.get<int>("max-disparity");
 	int window_size = parser.get<int>("window_size");
-	int tranwin_size = parser.get<int>("tranwin_size");
 
 	Mat dis;
 	Mat left = imread(leftname, 0); //read images into grayscale
 	Mat right = imread(rightname, 0);
 	int64 t2 = getTickCount();
-	Stereo s(window_size, max_disparity, tranwin_size);
+	Stereo s(window_size, max_disparity);
 	dis = s.stereo_match(left, right);
 
 	t2 = getTickCount() - t2;
